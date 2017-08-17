d9b2d5999b3ef048f813df085e27d8b572129408
Paulo Menezes <phgm@cesar.org.br>
2017-08-17 12:53:22 -0300

    isPrime

---
+++ coding-easy/isPrime.js
@@ -0,0 +1,27 @@
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  let prime = true;
+
+  for (var i = 2; i < number; i++) {
+    if (number % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+
+  return prime;
+}
+
+import { test } from 'ava';
+
+test(t => t.is(isPrime(0), false));
+test(t => t.is(isPrime(1), false));
+test(t => t.is(isPrime(2), true));
+test(t => t.is(isPrime(9), false));
+test(t => t.is(isPrime(17), true));
+test(t => t.is(isPrime(25), false));
+test(t => t.is(isPrime(73), true));
+test(t => t.is(isPrime(10000000000000), false));
